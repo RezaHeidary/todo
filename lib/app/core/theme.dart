6aed0fa1df7274ed76b6307f9ba02e1cd1887119
RezaHeidary <rrezah4338@gmail.com>
2025-08-10 16:09:45 +0330

    add theme

---
+++ lib/app/core/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Themes {
+  static final lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    primarySwatch: Colors.blue);
+ 
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/app/core/theme.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+Future<void> main() async {
+  //await initial(); 
+  //await Get.find<MatrialAppService>().getInit();
   runApp(
-    GetMaterialApp(
-      debugShowCheckedModeBanner: false,
+    const MyApp(),
+  );
+}
 
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),  
-      title: "Application",
+
+
+
+// Future initial() async {
+//   Get.put(MatrialAppService());
+// }
+
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+     // locale: const Locale("fa"),
+     // translationsKeys: AppTranslation.translations,
+      debugShowCheckedModeBanner: false,
+      theme: Themes.lightTheme,
+      title: "Hydro Machine",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-    ),
-  );
+    );
+  }
 }
